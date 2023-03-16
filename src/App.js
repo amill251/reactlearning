c265dabc7a1e8b4d05590e37135eb62828e6eff6
Alex Miller <alex@life-events.com>
2023-03-16 13:10:20 -0400

    part2e_1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { useState, useEffect } from 'react';
+import './styles.css';
 import personsService from './services/Service';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import Notification from './components/Notification';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
+
+  const [notificationMessage, setNotificationMessage] = useState(null);
 
   useEffect(() => {
     personsService.getAll().then((response) => {
@@ -34,24 +38,19 @@ const App = () => {
     const existingPerson = persons.find((person) => person.name === newName);
 
     if (existingPerson) {
-      if (
-        window.confirm(
-          `${newName} is already added to phonebook, replace the old number with a new one?`
-        )
-      ) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
         const updatedPerson = { ...existingPerson, number: newNumber };
 
-        personsService
-          .update(existingPerson.id, updatedPerson)
-          .then((returnedPerson) => {
-            setPersons(
-              persons.map((person) =>
-                person.id !== existingPerson.id ? person : returnedPerson.data
-              )
-            );
-            setNewName('');
-            setNewNumber('');
-          });
+        personsService.update(existingPerson.id, updatedPerson).then((returnedPerson) => {
+          setPersons(
+            persons.map((person) =>
+              person.id !== returnedPerson.data.id ? person : returnedPerson.data
+            )
+          );
+          setNewName('');
+          setNewNumber('');
+          showNotification(`Updated ${newName}`);
+        });
       }
     } else {
       const newPerson = {
@@ -59,10 +58,11 @@ const App = () => {
         number: newNumber,
       };
 
-      personsService.create(newPerson).then((response) => {
-        setPersons(persons.concat(response.data));
+      personsService.create(newPerson).then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson.data));
         setNewName('');
         setNewNumber('');
+        showNotification(`Added ${newName}`);
       });
     }
   };
@@ -73,19 +73,27 @@ const App = () => {
     if (window.confirm(`Delete ${personToDelete.name}?`)) {
       personsService.deletePerson(id).then(() => {
         setPersons(persons.filter((person) => person.id !== id));
+        showNotification(`Deleted ${personToDelete.name}`);
       });
     }
   };
 
   const filteredPersons = searchTerm
     ? persons.filter((person) =>
-      person.name.toLowerCase().includes(searchTerm.toLowerCase())
-    )
+      person.name.toLowerCase().includes(searchTerm.toLowerCase()))
     : persons;
+
+  const showNotification = (message) => {
+    setNotificationMessage(message);
+    setTimeout(() => {
+      setNotificationMessage(null);
+    }, 5000);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notificationMessage} />
       <Filter searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
       <h3>Add a new</h3>
       <PersonForm
